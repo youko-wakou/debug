b64a57435e0b497de9cb9e653163a087ddcdb9ba
youko-wakou <appu2pudding@gmail.com>
2017-12-04 02:41:20 +0900

    Initial commit

---
+++ app/src/main/java/techacademy/wakou/youko/debug/MainActivity.java
@@ -0,0 +1,18 @@
+package techacademy.wakou.youko.debug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView = null;
+        textView.setText("Hello Android");
+    }
+}
