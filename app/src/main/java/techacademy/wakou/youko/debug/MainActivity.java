c9766fa2fc9366ad600f78ba20af13a47a49fbc3
youko-wakou <appu2pudding@gmail.com>
2017-12-04 03:00:13 +0900

    ログ出力

--- app/src/main/java/techacademy/wakou/youko/debug/MainActivity.java
+++ app/src/main/java/techacademy/wakou/youko/debug/MainActivity.java
@@ -2,6 +2,7 @@ package techacademy.wakou.youko.debug;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = (TextView)findViewById(R.id.textView);
+        Log.d("ANDROID",textView.toString());
+
         textView = null;
+        Log.d("ANDROID",textView.toString());
         textView.setText("Hello Android");
     }
 }
